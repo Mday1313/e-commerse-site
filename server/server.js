9797291d88ff7c368d5dbc5f15aa74502c897f90
Mday1313 <melissaday076@gmail.com>
2019-07-18 14:17:09 -0400

    routes for registration and login complete, backend only

--- server/server.js
+++ server/server.js
@@ -18,6 +18,9 @@ const{User} = require('./models/user');
 
 
 //==============USERS====================
+
+// app.get('/api/users/auth',)
+
 app.post('/api/users/register',(req,res)=>{
     const user = new User(req.body);
 
@@ -32,8 +35,22 @@ app.post('/api/users/register',(req,res)=>{
 
 app.post('/api/users/login',(req,res)=>{
     // find email
+    User.findOne({"email":req.body.email},(err,user)=>{
+        if(!user) return res.json({loginSuccess:false,message:'Auth failed, email not found'});
 
-    // check password
+        // check password
+        user.comparePassword(req.body.password, (err, isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:'Wrong password'});
+
+            // generate a token
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth',user.token).status(200).json({
+                    loginSuccess: true
+                })
+            })
+        })
+    })
 })
 
 
